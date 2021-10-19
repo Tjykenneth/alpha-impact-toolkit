1d1e33b978fc5d7137a829d44e8fd7267124640b
vishalhp01 <vishal.upwork.hp@gmail.com>
2021-10-19 15:10:17 +0530

    fix(menu): Change header color

--- packages/uikit/src/widgets/Menu/Menu.tsx
+++ packages/uikit/src/widgets/Menu/Menu.tsx
@@ -28,7 +28,8 @@ const StyledNav = styled.nav<{ showMenu: boolean }>`
   padding-right: 16px;
   width: 100%;
   height: ${MENU_HEIGHT}px;
-  background-color: ${({ theme }) => theme.colors.safariGreen3};
+  // background-color: ${({ theme }) => theme.colors.safariGreen3};
+  background: cornflowerblue;
   border-bottom: solid 2px rgba(133, 133, 133, 0.1);
   z-index: 20;
   transform: translate3d(0, 0, 0);
